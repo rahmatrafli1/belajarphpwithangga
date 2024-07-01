56c3f8849f83b9ed17fcf820b4ef162ad48dbe44
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-07-01 10:20:51 +0700

    Tipe Data Pada PHP

---
+++ tipedata.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = "Ini adalah string"; // string
+$b = 10; // integer
+$c = 10.5; // float
+$d = true; // boolean
+$e = [1, 2, 3, 4, 5, 6, 7, 8]; // array
+$f = null; // null
+
+echo var_dump($f);
